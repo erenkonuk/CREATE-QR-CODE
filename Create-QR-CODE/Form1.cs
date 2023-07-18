02457a658c167c9324fed1b15e1d3f6e55b2d94f
erenkonuk <136901974+erenkonuk@users.noreply.github.com>
2023-07-18 14:48:46 +0300

    Add project files.

---
+++ Create-QR-CODE/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace Create_QR_CODE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string sentence = textBox1.Text;
+            textBox1.Focus();
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(sentence, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qRCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(4);
+            pictureBox1.Image = qrCodeImage;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = " ";
+            textBox1.Focus();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
